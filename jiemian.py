2b2e2289702f6bd4f11a6f6109378a2f768426da
AGUIAtNight <72181488+AGUIAtNight@users.noreply.github.com>
2023-12-07 14:21:55 +0800

    Add files via upload

--- jiemian.py
+++ jiemian.py
@@ -1,2 +1,19 @@
 import kivy
 from kivy.app import App
+
+from kivy.lang import Builder
+
+kv = '''
+BoxLayout:
+    Button:
+        text: 'Click Me'
+'''
+
+Builder.load_string(kv)
+
+class MyApp(App):
+    def build(self):
+        return Builder.load_string(kv)
+    
+if __name__ == '__main__':
+    MyApp().run()    
